bd7334342f8fc8fbcc7dad1b5b707940e2cd83ce
ShiomiQwQ <admin@shiomi.xyz>
2025-06-19 23:48:26 +0800

    Update xmlrpc.php

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,5 +1,6 @@
 Haha, you've been scammed
 This website doesn't use WordPress, so it's even more impossible to steal the administrative privileges of this website
 The vulnerability you used is so old that there are many countermeasures to deal with you
+And I have no plans to use WordPress in the future
 Such crawling is meaningless and takes up my bandwidth
 Bye
