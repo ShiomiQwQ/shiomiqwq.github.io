0c99c70f80c1922f335b5585ac3069325a02c7fe
ShiomiQwQ <admin@shiomi.xyz>
2025-06-19 23:45:31 +0800

    Create wp-login.php

---
+++ wp-login.php
@@ -0,0 +1,5 @@
+Haha, you've been scammed
+This website doesn't use WordPress, so it's even more impossible to steal the administrative privileges of this website
+The vulnerability you used is so old that there are many countermeasures to deal with you
+Such crawling is meaningless and takes up my bandwidth
+Bye
